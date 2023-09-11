d5e2f10021830db9b33d7e22396240791339ba0e
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-09-11 13:26:55 -0400

    Create 112.rb

---
+++ 112.rb
@@ -0,0 +1,18 @@
+# QUESTION 1
+# Write code to print hellohellohello using string addition.
+# Then write code to print hellohellohello again, this time using string multiplication.
+
+string = 'hello'
+puts string + string + string
+puts string * 3
+
+
+# QUESTION 2
+# Read the code below and predict which lines will crash. 
+# Then run the code and remove the invalid lines one at a time, until only the valid code remains.
+puts 9 + 3
+puts "9" + "3"
+puts 9 - 3
+puts 9 * 3
+puts "9" * 3
+puts 9 / 3
