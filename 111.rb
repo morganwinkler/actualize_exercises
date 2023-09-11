4d010abe1a97155ef0fbb835cb653b16cf4bf58c
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-09-11 13:23:37 -0400

    Create 111.rb

---
+++ 111.rb
@@ -0,0 +1,19 @@
+# QUESTION 1
+# Write code to print out the numbers 4, 30, 12, 983 on separate lines. 
+# For this exercise, you are required to use addition, subtraction, multiplication, and division at least once.
+puts 11 - 7
+puts 5 * 6
+puts 6 + 6
+puts 1966 / 2
+
+# QUESTION 2
+# Write code using addition, subtraction, multiplication, or division with numbers that causes the computer to crash.
+# Add a comment with a copy of the error message.
+# Add a comment that describes the error message in your own words.
+
+puts 4 x 3
+
+# 111.rb:14: syntax error, unexpected tIDENTIFIER, expecting end-of-input
+# puts 4 x 3
+
+# mistake made in line 14 
