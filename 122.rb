b4ca0c0ef3476fb7ddd75a84db6a373d91f4b207
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-09-12 12:55:04 -0400

    Uncomment first exercise

--- 122.rb
+++ 122.rb
@@ -2,11 +2,11 @@
 # Write code that asks the user for their name, then asks for their favorite color, 
 # then prints out back to them their name and favorite color in a sentence.
 
-# puts 'Hi, what is your name?'
-# user_name = gets.chomp.to_s.capitalize
-# puts '..and your favorite color?'
-# fav_color = gets.chomp.to_s.capitalize
-# puts "Nice to meet you, #{user_name}! #{fav_color} is a great choice :) "
+puts 'Hi, what is your name?'
+user_name = gets.chomp.to_s.capitalize
+puts '..and your favorite color?'
+fav_color = gets.chomp.to_s.capitalize
+puts "Nice to meet you, #{user_name}! #{fav_color} is a great choice :) "
 
 # QUESTION 2
 # The following code has a mistake on one line that doesn't cause the program to crash,
