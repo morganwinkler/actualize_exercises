9f7694314964fc070bf57b9620c68efda3de831c
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-09-12 12:45:44 -0400

    Create 121.rb

---
+++ 121.rb
@@ -0,0 +1,15 @@
+# QUESTION 1
+# Write example code to demonstrate three string methods you've never 
+# worked with before from the Ruby documentation. 
+# Write a comment for each method describing how it works in your own words.
+string = 'hello'
+
+puts string.center(100)
+puts string.count('l')
+puts string.partition('ll')
+
+# QUESTION 2
+# Write example code that crashes using an integer method from the Ruby documentation. 
+# Write a comment describing the error message in your own words.
+
+puts 7.3.odd?
