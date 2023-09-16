96c7a2fde66f8ab9d0ece2155cbb48da87e570c9
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-09-16 13:51:06 -0400

    Create 142.rb

---
+++ 142.rb
@@ -0,0 +1,22 @@
+# QUESTION 1
+# Write a program that starts with an array of strings, 
+# then prints out each string on separate lines using a while loop.
+
+array = ['pup', 'lake', 'house']
+index = 0
+while index < array.length
+    puts array[index]
+index = index + 1
+end
+
+
+# QUESTION 2
+# Write a program that starts with an array of numbers, 
+# then prints out each number times 3 using a while loop.
+
+array = [1, 4, 7, 34, 100]
+index = 0
+while index < array.length
+    puts array[index] * 3
+index = index + 1
+end
