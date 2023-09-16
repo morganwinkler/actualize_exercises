a506a25ccf88556e84ad68054db54e557e93ae44
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-09-16 14:05:12 -0400

    Create 144.rb

---
+++ 144.rb
@@ -0,0 +1,22 @@
+# QUESTION 1
+# Make an array called cars with three hashes to define three different cars. 
+# Each car should have a make (brand), model, color, and year. 
+# (Example: a silver 2006 Honda Accord, Honda is the make, Accord is the model)
+cars = [
+    {"make" => "hyundai", "model" => "sonota", "year" => 2011},
+    {"make" => "ford", "model" => "explorer", "year" => 2004},
+    {"make" => "dodge", "model" => "ram", "year" => 2022}
+]
+
+print cars
+
+
+# QUESTION 2
+# The following code should print the age of the first student in the array.
+# However, the code doesn't work. Fix the mistakes in the code.
+students = [
+  { "name" => "Maria", "age" => 20, "major" => "Computer Science" },
+  { "name" => "Sofia", "age" => 21, "major" => "Mathematics" },
+  { "name" => "Jayden", "age" => 19, "major" => "Biology" }
+]
+puts students[0]["age"]
