c5862bfe94e808502baa2c1d5e3e768121d6a841
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-09-16 13:47:01 -0400

    Create 141.rb

---
+++ 141.rb
@@ -0,0 +1,20 @@
+# QUESTION 1
+# Finish annotating the code below.
+x = 0                                  # 1
+names = ["Saron", "Majora", "Danilo"]  # 2
+names[1] = "Bob"                       # 3
+x = names[2]                           # 4
+names[0] = x                           # 5
+pp names                               # 6
+
+# 1: x is 0
+# 2: x is 0, names is ["Saron", "Majora", "Danilo"]
+# 3: x is 0, names is ["Saron", "Bob", "Danilo"]
+# 4: x is "Danilo" , names is ["Saron", "Bob", "Danilo"]
+# 5: x is "Danilo", names is ["Danilo", "Bob", "Danilo"]
+# 6: prints names to screen
+
+# QUESTION 2
+# Find and fix the mistakes in the code below.
+items = [3, 2, 32]
+pp items
