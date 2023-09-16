6a274325eacbc9df69d70f6938ef49e87c0f386a
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-09-16 13:40:57 -0400

    Create 134.rb

---
+++ 134.rb
@@ -0,0 +1,28 @@
+# QUESTION 1
+# Write code using a while loop that asks the user to enter a number 
+# and will run forever until the user enters a number greater than 10.
+
+while true
+    puts 'Please enter a number'
+    number = gets.chomp.to_i
+    if number <= 10
+    puts 'Try agin'
+    else
+        break
+    end
+end
+
+# QUESTION 2
+# The following code will run 100 times, and each time it will ask the user for their name. 
+# The code will break early if the user's name is Bob.
+# Rewrite the code using a while loop so the program will run *forever* unless 
+# the user enters a name of Bob.
+
+while true
+  puts "What is your name?"  
+  name = gets.chomp
+  if name == "Bob"
+    break
+  end
+end
+puts "Hi, Bob!"
