b7d90c566648e8e7c250df241169f3501faef123
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-09-19 14:13:15 -0400

    Create 221.rb

---
+++ 221.rb
@@ -0,0 +1,21 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Make an array of hashes to store the first name and last name for 3 different people.
+#  Then print out the first person's info.
+
+peeps = [
+    {'first_name' => 'John', 'last_name' => 'Doe'},
+    {'first_name' => 'Jane', 'last_name' => 'Smith'},
+    {'first_name' => 'Chuck', 'last_name' => 'Norris'}
+]
+
+print peeps[0]
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# I was not. I forgot that the keys had to be in paranthesis as well
+# so I kept throwing an error code and eventually looked at the solution
