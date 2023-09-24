d6c9c60e76be3015d3dc7f2f92a12d3c8e733579
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-09-24 13:11:00 -0400

    Create 242.rb

---
+++ 242.rb
@@ -0,0 +1,19 @@
+# QUESTION 1
+# Use a variable to store an array with three different words.
+# Then add one more word to the array and print the array on one line.
+
+words = ['hey', 'hi', 'hello']
+words.push 'sup'
+pp words
+
+# QUESTION 2
+# Use a variable to store a hash with two different words and their definitions.
+# Then add one more word with its definition and print the hash on one line.
+
+words = {
+    'dog' => 'fluffy boy',
+    'bark' => 'woof'
+}
+words['shake'] = 'good boy'
+pp words
+
