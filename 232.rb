62605d7e4fc6a2ec6bd9cbb4759383dcae6e582a
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-09-24 12:45:16 -0400

    Create 232.rb

---
+++ 232.rb
@@ -0,0 +1,18 @@
+# QUESTION 1
+# Write code that prints all the years between 1980 to 2020 (including 1980 and 2020).
+
+i = 1980
+while i <= 2020
+    puts i 
+    i += 1
+end
+
+
+# QUESTION 2
+# The following code is supposed to print the numbers 1 through 10, but it has errors.
+# Fix the errors with the code below.
+count = 0
+while count < 10
+    count +=1
+  puts count
+end
