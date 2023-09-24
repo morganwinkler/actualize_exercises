fafdba12b287e36aea645cc18e566d0b4460a46d
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-09-24 12:41:50 -0400

    Create 231.rb

---
+++ 231.rb
@@ -0,0 +1,21 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Write an Account class with attributes for name and balance.
+
+class Account
+    def initialize(name, balance)
+        @name = name
+        @balance = balance
+    end
+end
+
+checking_account = Account.new('Checking', 5000)
+pp checking_account
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+# I was able to do it from memory. For the next deliberate question I want to do
+# classes still but a harder one
+
