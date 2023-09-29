770b689bfc5204228ff3c35e15530dd928710052
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-09-29 17:07:10 -0400

    Create 323.rb

---
+++ 323.rb
@@ -0,0 +1,10 @@
+# QUESTION 1
+# Create a program that multiplies by 2 until 100,000. 
+# That is, the program should print out: 1, 2, 4, 8, 16, 32, etc. until you get above 100,000.
+
+
+num = 1
+until num > 100000
+   num = num * 2
+   puts num
+end
