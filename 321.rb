d81b9b03722aaf27f5a2e206e1bd3b2749d26154
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-09-29 17:02:13 -0400

    Create 321.rb

---
+++ 321.rb
@@ -0,0 +1,23 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Start with an array of strings and compute the total length of all the strings.
+# For example, ["volleyball", "basketball", "badminton"] becomes 29.
+
+array = ["volleyball", "basketball", "badminton"]
+
+index = 0
+sum = 0
+while index < array.length
+    sum += array[index].length
+index = index + 1
+end
+puts sum
+
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+# I was able to do this one without looking at the solution. Tomorrow I am going to
+# stay in the same section and try to challenge myself
