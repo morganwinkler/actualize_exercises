fe7ffc70e1348745e4092df2469d24843b1b3181
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-10-02 11:14:39 -0400

    Create 351.rb

---
+++ 351.rb
@@ -0,0 +1,17 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Make an array of hashes to store the brand and model for 3 different laptops. 
+# Then print out the second laptop's model.
+
+laptops = [
+  { "brand" => "Microsoft", "model" => "Surface" },
+  { "brand" => "Apple", "model" => "MacBook Pro" },
+  { "brand" => "Dell", "model" => "XPS 13" }
+]
+pp laptops[1]["model"]
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
