bfd2ac9fbc6d4f78303f021fa84ced7ee57765b7
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-10-04 14:23:19 -0400

    Create 434.rb

---
+++ 434.rb
@@ -0,0 +1,18 @@
+# QUESTION 1
+# Read about regular expressions in Ruby:
+# http://ruby-for-beginners.rubymonstas.org/advanced/regular_expressions.html 
+# Then write a program that takes in a string and prints out 
+# the total number of numbers found in the string using regular expressions. 
+# For example, given the string "Pg7USm29ln", the program would print 3 since
+# there are 3 total numbers (7, 2, and 9) in the string.
+
+
+string = 'Pg7USm29ln'
+string2 = string.split('')
+
+index = 0
+counter = 0
+while index < string2.length
+    character = string2[index]
+
+end
