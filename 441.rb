47d26df4bd3c66515b57b87802fbf2893d586c40
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-10-05 14:03:20 -0400

    Create 441.rb

---
+++ 441.rb
@@ -0,0 +1,19 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Start with an array of numbers and create a new array with each number converted into a string.
+# For example, [1, 2, 3] becomes ["1", "2", "3"]
+
+numbers = [1, 2, 3]
+string_numbers = []
+numbers.each do |number|
+    string_numbers << number.to_s
+end
+
+pp string_numbers
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
