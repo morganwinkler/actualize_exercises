ad46ddc31646c584439e54e8556c35fcb90fa1a1
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-10-13 10:40:38 -0400

    Add exercise solutions

---
+++ 543.js
@@ -0,0 +1,25 @@
+// QUESTION 1
+// Write code to store an array of numbers.
+// The print out the last item in that array.
+
+var words = ['hey', 'hi', 'hello']
+console.log(words[2])
+
+
+// QUESTION 2
+// Write code to store a number in a variable.
+// Then write a condition that 
+// prints -1 if the number is less than 10, 
+// prints 1 if the number is greater than 10, 
+// and prints 0 if the number is equal to 10.
+
+var number = 9;
+
+if (number < 10) {
+    console.log(-1) 
+}
+else if (number > 10) {
+    console.log(1)
+}
+else 
+    {console.log(0)}
